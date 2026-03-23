d220517179e40a17b43c168563989eb4855d8d2f
uppuvamshika2001-prog <uppuvamshika2001@gmail.com>
2026-03-23 12:22:24 +0530

    fix: load dotenv dynamically in development env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+    import('dotenv').then(d => d.config());
+}
 
 export const config = {
     // Server
